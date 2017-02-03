e87d33991fb7e53761d1550f2ccddae0b3b0569f
Adam Wang <adamw3494@gmail.com>
2017-02-03 17:32:47 -0500

    commit new problems

---
+++ wk2.js
@@ -0,0 +1,43 @@
+// """Remove Linked List Element
+//
+// Remove all elements from a linked list of integers that have value val.
+// Example
+// Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+// Return: 1 --> 2 --> 3 --> 4 --> 5"""
+
+function removeLinkListVal(list, value) {
+
+}
+
+
+
+// """Sorted Insert
+//
+// Given a sorted stack and an element, insert the element into the correct position of the stack. The stack will have the methods: peek, isEmpty, push, and pop."""
+
+var stack = function(value) {
+  
+}
+
+
+
+// """Sort Stack
+//
+// Takes an unsorted stack and sort it. You may only use stacks (& the call stack) as a storage mechanism."""
+
+
+
+
+
+// 	"""Container With Most Water
+//
+// Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+// Note: You may not slant the container."""
+
+
+
+
+
+// """Given a binary tree, find its maximum depth.
+//
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node."""
