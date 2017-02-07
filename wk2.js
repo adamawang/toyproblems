4772006b09f01844530671ed705e04e9842d5721
Adam Wang <adamw3494@gmail.com>
2017-02-06 20:23:56 -0500

    todo update

--- wk2.js
+++ wk2.js
@@ -60,12 +60,15 @@ function removeLinkListVal(list, value) {
   // traverse the list and when you find a matching value in 'node.next', set current node.next equal to node.next.next to skip it
   function traverse(node) {
     // if at end of list and node.next.next is null, return
-    if(node.next.next === null){
-      return;
-    }
-    if(node.next.value === value){
-      node.next = node.next.next;
-    }
+
+// TODO Fix logic here... need to catch if .next is null or will get cannot read .next property of null
+
+    // if(node.next.next === null){
+    //   return;
+    // }
+    // if(node.next.value === value){
+    //   node.next = node.next.next;
+    // }
     traverse(node.next);
   }
   traverse(list.head);
