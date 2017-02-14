025a71fd00fb8e9e4bb3f5d844940e9a53fc632f
Adam Wang <adamw3494@gmail.com>
2017-02-14 12:20:19 -0500

    wk4 problems

---
+++ wk4.js
@@ -0,0 +1,71 @@
+// """Given two binary trees, write a function to check if they are equal or not.
+//
+// Two binary trees are considered equal if they are structurally identical and the nodes have the same value."""
+
+function binaryTreeCheck(tree1, tree2) {
+
+}
+
+
+
+// """House Robber
+//
+// You are a professional robber planning to rob houses along a street.
+// Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system
+// connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+//
+// Given a list of non-negative integers representing the amount of money of each house,
+// determine the maximum amount of money you can rob tonight without alerting the police."""
+
+var houses = [1,2,3,4,5,6,7,8,9];
+
+function houseRobber(array) {
+  // naive solution: check every other house and add up totals, whichever one is highest, choose those houses
+}
+
+
+
+// """Odd - Even Linked List
+//
+// Given a singly linked list, group all odd nodes together followed by the even nodes.
+// Please note here we are talking about the node number and not the value in the nodes.
+//
+// You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+//
+// Example:
+// Given 1->2->3->4->5->NULL,
+// return 1->3->5->2->4->NULL.
+//
+// Note:
+// The relative order inside both the even and odd groups should remain as it was in the input.
+// The first node is considered odd, the second node even and so on ..."""
+
+function evenLinkedList(list) {
+  
+}
+
+
+
+// """Single Number
+//
+// Given an array of integers, every element appears three times except for one. Find that single one.
+// """
+
+function singleNumber(array) {
+
+}
+
+
+
+
+// """Single Number II
+//
+// Given an array of integers, every element appears three times except for one. Find that single one.
+//
+// Note:
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+// """
+
+function singleNumLinear(array) {
+
+}
